15af18e3c1496c4207abda6ae2294580c62ff132
Nuris Cuellar <abbyramos198@gmail.com>
2022-08-11 19:54:58 -0600

    Semana 1

---
+++ Script-Practica01.R
@@ -0,0 +1,24 @@
+2*(3+4)^2
+sqrt(16)
+abs(-97.6) # abs(x) calcula el valor absoluto de x
+x = 4 # almacena el valor de 4 en la variable x
+x # Muestra el contenido de la variable x
+sqrt(x)-3/2
+p <- (4 > 8)
+p
+q = -6+4 < 3 && 4 != 10
+q
+r = -6+4 > 3 || 4 == 10
+r
+t <- !r
+t
+sin(pi/2)
+(y=cos(pi)) # Los primeros paréntesis permiten ver el valor calculado de y
+
+
+log(3) # Calcula el logaritmo natural de 3
+log10(8) # Calcula el logaritmo base 10 de 8
+# La sintaxis general es: logb(x, base)
+logb(16, 7)
+# exp() cálcula la función exponencial
+exp(1)
