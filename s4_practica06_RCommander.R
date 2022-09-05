74c7fd9367525e97fd56f70501f64b4033f6d564
Nuris Cuellar <abbyramos198@gmail.com>
2022-09-04 22:53:18 -0600

    Semana 4

--- s4_practica06_RCommander.R
+++ s4_practica06_RCommander.R
@@ -1,6 +1,6 @@
 
 consumo <- 
-  read.table("C:/Users/abbyc/Desktop/Ciclo II 2022/Análisis estadístico con R/curso-R-2022/Consumo.txt",
+  read.table("C:/Users/abbyc/Desktop/Ciclo II 2022/An?lisis estad?stico con R/curso-R-2022/Consumo.txt",
    header=TRUE, stringsAsFactors=TRUE, sep="", na.strings="NA", dec=".", 
   strip.white=TRUE)
 summary(consumo)
@@ -8,4 +8,3 @@ with(consumo, Barplot(PC, xlab="PC", ylab="Frequency", label.bars=TRUE))
 library(colorspace, pos=18)
 with(consumo, piechart(PC, xlab="", ylab="", main="PC", col=rainbow_hcl(3), 
   scale="percent"))
-
